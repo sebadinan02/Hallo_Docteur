44df0100890bb58f8cb1d3fe2034015094529d77
JfrancoisZouba <jfrancoiszouba72@gmail.com>
2020-12-30 21:40:28 +0000

    Travail de Mr Franck

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var checkBox = document.querySelector('#check');
-checkBox.addEventListener('click', function () {
-	let mybody=document.querySelector('body')
-	if (checkBox.checked==true) {
-		mybody.style.overflow="hidden";
-	} else{
-		mybody.style.overflow="";
-	}
-});
