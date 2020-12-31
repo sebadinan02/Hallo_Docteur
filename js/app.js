dc55fdf93d5c226641a589329e47608d232f3833
JfrancoisZouba <jfrancoiszouba72@gmail.com>
2020-12-31 13:15:27 +0000

    Page de RDV/formulaires terminées

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-var checkBox = document.querySelector('#check');
+let checkBox = document.querySelector('#check');
+let sidebar=document.querySelector('.sidebar');
+sidebar.style.zIndex =1000;
 checkBox.addEventListener('click', function () {
 	let mybody=document.querySelector('body')
+	let btn_nav =document.querySelector('#btn-nav');
 	if (checkBox.checked==true) {
 		mybody.style.overflow="hidden";
+		btn_nav.style.left="250";
+		btn_nav.style.opacity="0";
 	} else{
 		mybody.style.overflow="";
+		btn_nav.style.left="0";
+		btn_nav.style.opacity="1";
 	}
 });
