e317b3a3883de6b9df198e9dc6450ca7bedc9104
JfrancoisZouba <jfrancoiszouba72@gmail.com>
2021-01-02 14:41:15 +0000

    Page accueil avec responsive en cours de déploiement

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 let checkBox = document.querySelector('#check');
-let sidebar=document.querySelector('.sidebar');
-sidebar.style.zIndex =1000;
 checkBox.addEventListener('click', function () {
-	let mybody=document.querySelector('body')
 	let btn_nav =document.querySelector('#btn-nav');
+	let mybody=document.querySelector('body');
 	if (checkBox.checked==true) {
-		mybody.style.overflow="hidden";
 		btn_nav.style.left="250";
 		btn_nav.style.opacity="0";
+		mybody.style.overflow='hidden'
+		
 	} else{
-		mybody.style.overflow="";
+		mybody.style.overflow=''
 		btn_nav.style.left="0";
-		btn_nav.style.opacity="1";
+		btn_nav.style.opacity="";
 	}
 });
